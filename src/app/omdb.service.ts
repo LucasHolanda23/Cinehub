cafe17fe56705b788cd44ed5b38d1d5623e52d1e
Maria Carolina Melo <carolmelo82@hotmail.com>
2025-11-07 19:47:18 -0300

    ajuste2

--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -6,18 +6,14 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class OmdbService {
- 
+export class OmdbService {  
   private readonly API_KEY = 'a5bacd94'; 
   private readonly BASE_URL = 'https://www.omdbapi.com/';
 
   constructor(private http: HttpClient) { }
 
-
   buscarFilmePorTitulo(title: string): Observable<any> {
     const url = `${this.BASE_URL}?t=${encodeURIComponent(title)}&apikey=${this.API_KEY}`;
-    
-    // O Service RETORNA o Observable.
     return this.http.get(url);
   }
 }
