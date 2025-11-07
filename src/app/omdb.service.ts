97ef1f0212a8e5eebe98ed818555ec1e5eb06bfe
Maria Carolina Melo <carolmelo82@hotmail.com>
2025-11-07 19:41:50 -0300

    ajuste

--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -3,27 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
-// O 'providedIn: root' registra este serviço na raiz da aplicação
-// e permite que seja injetado em qualquer lugar.
-
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
-  // Sua chave de API está definida aqui
+ 
   private readonly API_KEY = 'a5bacd94'; 
   private readonly BASE_URL = 'https://www.omdbapi.com/';
 
-  // Injeção do HttpClient: ESSA LINHA É O REQUISITO PRINCIPAL
-  // Ela agora está no Service, e não no Componente.
   constructor(private http: HttpClient) { }
 
-  /**
-   * Realiza uma requisição GET na API da OMDB pelo título do filme.
-   * A lógica de conexão (HttpClient) está alocada aqui, no Service.
-   * @param title O título do filme a ser buscado.
-   * @returns Um Observable com a resposta da API.
-   */
+
   buscarFilmePorTitulo(title: string): Observable<any> {
     const url = `${this.BASE_URL}?t=${encodeURIComponent(title)}&apikey=${this.API_KEY}`;
     
